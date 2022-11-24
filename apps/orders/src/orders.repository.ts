8638aa35ec707b9c351b0f108d15353cbdda9c88
marvin espira <espiramarvin@gmail.com>
2022-11-24 09:54:20 +0300

    feat: updates on order

---
+++ apps/orders/src/orders.repository.ts
@@ -0,0 +1,19 @@
+// order repository allows us to manipulate data inside the database
+
+import { AbstractRepository } from '@app/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectConnection, InjectModel } from '@nestjs/mongoose';
+import { Connection, Model } from 'mongoose';
+import { Order } from './schema/order.schema';
+
+@Injectable()
+export class OrdersRepository extends AbstractRepository<Order> {
+  protected readonly logger = new Logger(OrdersRepository.name);
+
+  constructor(
+    @InjectModel(Order.name) orderModel: Model<Order>,
+    @InjectConnection() connection: Connection,
+  ) {
+    super(orderModel, connection); // this gets all the CRUD functionality from the abstract repository
+  }
+}
