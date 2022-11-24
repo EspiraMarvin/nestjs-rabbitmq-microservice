b94c38b0d37aa8a664bb83f8c8bb9a8557db55bd
marvin espira <espiramarvin@gmail.com>
2022-11-24 09:55:28 +0300

    updates

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,2 +1,10 @@
-export * from './common.module';
-export * from './common.service';
+// export * from './common.module';
+// export * from './common.service';
+
+export * from './database/abstract.repository';
+export * from './database/database.module';
+export * from './database/abstract.schema';
+// export * from './rmq/rmq.service';
+// export * from './rmq/rmq.module';
+// export * from './auth/auth.module';
+// export * from './auth/jwt-auth.guard';
